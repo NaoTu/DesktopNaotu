a7e0f004b491c53e6bace09ea22763f22a33f64d
AnClark Liu <clarklaw4701@qq.com>
2020-10-21 22:09:38 +0800

    shortcut: Add I18n support for shortcut dialog

--- app/src/ui/shortcut.ts
+++ app/src/ui/shortcut.ts
@@ -1,9 +1,11 @@
+import { I18n } from "../core/i18n";
+
 export function shortcutDialog() {
   let html = `
   <div class="modal-body shortcut-dialog ng-scope">
     <!-- ngRepeat: group in shortcut -->
     <section ng-repeat="group in shortcut" class="ng-scope">
-      <h3 class="ng-binding">节点操作</h3>
+      <h3 class="ng-binding">${ I18n.__("dShortcut_NodeOperation") }</h3>
       <!-- ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
         <span
@@ -11,7 +13,7 @@ export function shortcutDialog() {
           ng-bind-html="genKeyDOM(k.key) | trusted"
           ><span class="shortcut-key Enter" title="Enter">Enter</span></span
         >
-        <span class="description ng-binding"> 插入兄弟节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_InsertSiblingNode") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -21,7 +23,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Tab" title="Tab">Tab</span>,
           <span class="shortcut-key Insert" title="Insert">Insert</span></span
         >
-        <span class="description ng-binding"> 插入子节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_InsertChildNode") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -31,7 +33,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Shift" title="Shift">Shift</span> +
           <span class="shortcut-key Tab" title="Tab">Tab</span></span
         >
-        <span class="description ng-binding"> 插入父节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_InsertParentNode") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -40,7 +42,7 @@ export function shortcutDialog() {
           ng-bind-html="genKeyDOM(k.key) | trusted"
           ><span class="shortcut-key Delete" title="Delete">Delete</span></span
         >
-        <span class="description ng-binding"> 删除节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_RemoveNode") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -52,7 +54,7 @@ export function shortcutDialog() {
           <span class="shortcut-key Left" title="Left">Left</span>,
           <span class="shortcut-key Right" title="Right">Right</span></span
         >
-        <span class="description ng-binding"> 节点导航</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_Navigation") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -63,7 +65,7 @@ export function shortcutDialog() {
           <span class="shortcut-key Up" title="Up">Up</span>,
           <span class="shortcut-key Down" title="Down">Down</span></span
         >
-        <span class="description ng-binding"> 向上/向下调整顺序</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_ReorderNodeUpDown") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -72,7 +74,7 @@ export function shortcutDialog() {
           ng-bind-html="genKeyDOM(k.key) | trusted"
           ><span class="shortcut-key /" title="/">/</span></span
         >
-        <span class="description ng-binding"> 展开/收起节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_ExpandOrCollapse") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -81,7 +83,7 @@ export function shortcutDialog() {
           ng-bind-html="genKeyDOM(k.key) | trusted"
           ><span class="shortcut-key F2" title="F2">F2</span></span
         >
-        <span class="description ng-binding"> 编辑节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_EditNode") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -91,7 +93,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Shift" title="Shift">Shift</span> +
           <span class="shortcut-key Enter" title="Enter">Enter</span></span
         >
-        <span class="description ng-binding"> 文本换行</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_TextNewline") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -101,7 +103,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key A" title="A">A</span></span
         >
-        <span class="description ng-binding"> 全选节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_SelectAll") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -111,7 +113,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key C" title="C">C</span></span
         >
-        <span class="description ng-binding"> 复制节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_CopyNode") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -121,7 +123,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key X" title="X">X</span></span
         >
-        <span class="description ng-binding"> 剪切节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_CutNode") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -131,7 +133,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key V" title="V">V</span></span
         >
-        <span class="description ng-binding"> 粘贴节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_PasteNode") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -141,7 +143,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key B" title="B">B</span></span
         >
-        <span class="description ng-binding"> 加粗</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_Bold") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -151,7 +153,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key I" title="I">I</span></span
         >
-        <span class="description ng-binding"> 斜体</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_Italic") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -161,47 +163,47 @@ export function shortcutDialog() {
           ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key F" title="F">F</span></span
         >
-        <span class="description ng-binding"> 查找节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_FindNode") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
     </section>
     <!-- end ngRepeat: group in shortcut -->
     <section ng-repeat="group in shortcut" class="ng-scope">
-      <h3 class="ng-binding">视野控制</h3>
+      <h3 class="ng-binding">${ I18n.__("dShortcut_ScopeControl") }</h3>
       <!-- ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
           ><span class="shortcut-key Alt" title="Alt">Alt</span> +
-          <span class="shortcut-key 拖动" title="拖动">拖动</span>,
-          <span class="shortcut-key 右键拖动" title="右键拖动"
-            >右键拖动</span
+          <span class="shortcut-key ${ I18n.__("dShortcut_Drag") }" title="${ I18n.__("dShortcut_Drag") }">${ I18n.__("dShortcut_Drag") }</span>,
+          <span class="shortcut-key ${ I18n.__("dShortcut_DragWithMouseRight") }" title="${ I18n.__("dShortcut_DragWithMouseRight") }"
+            >${ I18n.__("dShortcut_DragWithMouseRight") }</span
           ></span
         >
-        <span class="description ng-binding"> 拖动视野</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_DragScope") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key 滚轮" title="滚轮">滚轮</span>,
-          <span class="shortcut-key 触摸板" title="触摸板">触摸板</span></span
+          ><span class="shortcut-key ${ I18n.__("dShortcut_Mousewheel") }" title="${ I18n.__("dShortcut_Mousewheel") }">${ I18n.__("dShortcut_Mousewheel") }</span>,
+          <span class="shortcut-key ${ I18n.__("dShortcut_Touchpad") }" title="${ I18n.__("dShortcut_Touchpad") }">${ I18n.__("dShortcut_Touchpad") }</span></span
         >
-        <span class="description ng-binding"> 移动视野</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_MoveScope") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key 空白处双击" title="空白处双击"
-            >空白处双击</span
+          ><span class="shortcut-key ${ I18n.__("dShortcut_DblClickOnSpace") }" title="${ I18n.__("dShortcut_DblClickOnSpace") }"
+            > ${ I18n.__("dShortcut_DblClickOnSpace") }</span
           >, <span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key Enter" title="Enter">Enter</span></span
         >
-        <span class="description ng-binding"> 居中根节点</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_PlaceRootOnCentral") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -212,13 +214,13 @@ export function shortcutDialog() {
           <span class="shortcut-key +" title="+">+</span>,
           <span class="shortcut-key -" title="-">-</span></span
         >
-        <span class="description ng-binding"> 放大/缩小视野</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_ZoomScope") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
     </section>
     <!-- end ngRepeat: group in shortcut -->
     <section ng-repeat="group in shortcut" class="ng-scope">
-      <h3 class="ng-binding">布局</h3>
+      <h3 class="ng-binding">${ I18n.__("dShortcut_Layout") }</h3>
       <!-- ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
         <span
@@ -228,13 +230,13 @@ export function shortcutDialog() {
           <span class="shortcut-key Shift" title="Shift">Shift</span> +
           <span class="shortcut-key L" title="L">L</span></span
         >
-        <span class="description ng-binding"> 整理布局</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_PutLayoutInOrder") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
     </section>
     <!-- end ngRepeat: group in shortcut -->
     <section ng-repeat="group in shortcut" class="ng-scope">
-      <h3 class="ng-binding">后悔药</h3>
+      <h3 class="ng-binding">${ I18n.__("dShortcut_Regrets") }</h3>
       <!-- ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
         <span
@@ -243,7 +245,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key Z" title="Z">Z</span></span
         >
-        <span class="description ng-binding"> 撤销</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_Undo") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
       <div class="shortcut-group ng-scope" ng-repeat="k in group.groupItem">
@@ -253,7 +255,7 @@ export function shortcutDialog() {
           ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
           <span class="shortcut-key Y" title="Y">Y</span></span
         >
-        <span class="description ng-binding"> 重做</span>
+        <span class="description ng-binding"> ${ I18n.__("dShortcut_Redo") }</span>
       </div>
       <!-- end ngRepeat: k in group.groupItem -->
     </section>
@@ -262,7 +264,7 @@ export function shortcutDialog() {
   `;
 
   bootbox.dialog({
-    title: "快捷键",
+    title: I18n.__("dShortcut"),
     message: html
   });
 }
