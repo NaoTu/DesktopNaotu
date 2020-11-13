760806da9ec07a7d736d47db61c37c845c755494
AnClark <clarklaw4701@qq.com>
2020-11-13 20:38:54 +0800

    shortcut: 移除不必要的类
    原先的快捷键按钮中加入了与按钮标签同名的类，实属不必要，同时会影响布局。移除之。

--- app/src/ui/shortcut.ts
+++ app/src/ui/shortcut.ts
@@ -11,7 +11,7 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Enter" title="Enter">Enter</span></span
+          ><span class="shortcut-key" title="Enter">Enter</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_InsertSiblingNode") }</span>
       </div>
@@ -20,8 +20,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Tab" title="Tab">Tab</span>,
-          <span class="shortcut-key Insert" title="Insert">Insert</span></span
+          ><span class="shortcut-key" title="Tab">Tab</span>,
+          <span class="shortcut-key" title="Insert">Insert</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_InsertChildNode") }</span>
       </div>
@@ -30,8 +30,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Shift" title="Shift">Shift</span> +
-          <span class="shortcut-key Tab" title="Tab">Tab</span></span
+          ><span class="shortcut-key" title="Shift">Shift</span> +
+          <span class="shortcut-key" title="Tab">Tab</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_InsertParentNode") }</span>
       </div>
@@ -40,7 +40,7 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Delete" title="Delete">Delete</span></span
+          ><span class="shortcut-key" title="Delete">Delete</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_RemoveNode") }</span>
       </div>
@@ -49,10 +49,10 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Up" title="Up">Up</span>,
-          <span class="shortcut-key Down" title="Down">Down</span>,
-          <span class="shortcut-key Left" title="Left">Left</span>,
-          <span class="shortcut-key Right" title="Right">Right</span></span
+          ><span class="shortcut-key" title="Up">Up</span>,
+          <span class="shortcut-key" title="Down">Down</span>,
+          <span class="shortcut-key" title="Left">Left</span>,
+          <span class="shortcut-key" title="Right">Right</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_Navigation") }</span>
       </div>
@@ -61,9 +61,9 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Alt" title="Alt">Alt</span> +
-          <span class="shortcut-key Up" title="Up">Up</span>,
-          <span class="shortcut-key Down" title="Down">Down</span></span
+          ><span class="shortcut-key" title="Alt">Alt</span> +
+          <span class="shortcut-key" title="Up">Up</span>,
+          <span class="shortcut-key" title="Down">Down</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_ReorderNodeUpDown") }</span>
       </div>
@@ -72,7 +72,7 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key /" title="/">/</span></span
+          ><span class="shortcut-key" title="/">/</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_ExpandOrCollapse") }</span>
       </div>
@@ -81,7 +81,7 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key F2" title="F2">F2</span></span
+          ><span class="shortcut-key" title="F2">F2</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_EditNode") }</span>
       </div>
@@ -90,8 +90,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Shift" title="Shift">Shift</span> +
-          <span class="shortcut-key Enter" title="Enter">Enter</span></span
+          ><span class="shortcut-key" title="Shift">Shift</span> +
+          <span class="shortcut-key" title="Enter">Enter</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_TextNewline") }</span>
       </div>
@@ -100,8 +100,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key A" title="A">A</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="A">A</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_SelectAll") }</span>
       </div>
@@ -110,8 +110,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key C" title="C">C</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="C">C</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_CopyNode") }</span>
       </div>
@@ -120,8 +120,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key X" title="X">X</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="X">X</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_CutNode") }</span>
       </div>
@@ -130,8 +130,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key V" title="V">V</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="V">V</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_PasteNode") }</span>
       </div>
@@ -140,8 +140,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key B" title="B">B</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="B">B</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_Bold") }</span>
       </div>
@@ -150,8 +150,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key I" title="I">I</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="I">I</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_Italic") }</span>
       </div>
@@ -160,8 +160,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key F" title="F">F</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="F">F</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_FindNode") }</span>
       </div>
@@ -175,9 +175,9 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Alt" title="Alt">Alt</span> +
-          <span class="shortcut-key ${ I18n.__("dShortcut_Drag") }" title="${ I18n.__("dShortcut_Drag") }">${ I18n.__("dShortcut_Drag") }</span>,
-          <span class="shortcut-key ${ I18n.__("dShortcut_DragWithMouseRight") }" title="${ I18n.__("dShortcut_DragWithMouseRight") }"
+          ><span class="shortcut-key" title="Alt">Alt</span> +
+          <span class="shortcut-key" title="${ I18n.__("dShortcut_Drag") }">${ I18n.__("dShortcut_Drag") }</span>,
+          <span class="shortcut-key" title="${ I18n.__("dShortcut_DragWithMouseRight") }"
             >${ I18n.__("dShortcut_DragWithMouseRight") }</span
           ></span
         >
@@ -188,8 +188,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key ${ I18n.__("dShortcut_Mousewheel") }" title="${ I18n.__("dShortcut_Mousewheel") }">${ I18n.__("dShortcut_Mousewheel") }</span>,
-          <span class="shortcut-key ${ I18n.__("dShortcut_Touchpad") }" title="${ I18n.__("dShortcut_Touchpad") }">${ I18n.__("dShortcut_Touchpad") }</span></span
+          ><span class="shortcut-key" title="${ I18n.__("dShortcut_Mousewheel") }">${ I18n.__("dShortcut_Mousewheel") }</span>,
+          <span class="shortcut-key" title="${ I18n.__("dShortcut_Touchpad") }">${ I18n.__("dShortcut_Touchpad") }</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_MoveScope") }</span>
       </div>
@@ -198,10 +198,10 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key ${ I18n.__("dShortcut_DblClickOnSpace") }" title="${ I18n.__("dShortcut_DblClickOnSpace") }"
+          ><span class="shortcut-key" title="${ I18n.__("dShortcut_DblClickOnSpace") }"
             > ${ I18n.__("dShortcut_DblClickOnSpace") }</span
-          >, <span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key Enter" title="Enter">Enter</span></span
+          >, <span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="Enter">Enter</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_PlaceRootOnCentral") }</span>
       </div>
@@ -210,9 +210,9 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key +" title="+">+</span>,
-          <span class="shortcut-key -" title="-">-</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="+">+</span>,
+          <span class="shortcut-key" title="-">-</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_ZoomScope") }</span>
       </div>
@@ -226,9 +226,9 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key Shift" title="Shift">Shift</span> +
-          <span class="shortcut-key L" title="L">L</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="Shift">Shift</span> +
+          <span class="shortcut-key" title="L">L</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_PutLayoutInOrder") }</span>
       </div>
@@ -242,8 +242,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key Z" title="Z">Z</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="Z">Z</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_Undo") }</span>
       </div>
@@ -252,8 +252,8 @@ export function shortcutDialog() {
         <span
           class="shortcut ng-binding"
           ng-bind-html="genKeyDOM(k.key) | trusted"
-          ><span class="shortcut-key Ctrl" title="Ctrl">Ctrl</span> +
-          <span class="shortcut-key Y" title="Y">Y</span></span
+          ><span class="shortcut-key" title="Ctrl">Ctrl</span> +
+          <span class="shortcut-key" title="Y">Y</span></span
         >
         <span class="description ng-binding"> ${ I18n.__("dShortcut_Redo") }</span>
       </div>
